99955209ab7f40f53292a703899efe5f39657143
priit.mx.ee <priit@mx.ee>
2010-01-31 09:58:27 +0100

    start using bundler

--- rails/init.rb
+++ rails/init.rb
@@ -1,8 +1,4 @@
-if config.respond_to?(:gems)
-  config.gem 'ruby-openid', :lib => 'openid', :version => '>=2.0.4'
-else
-  puts 'Please upgrade Rails to 2.1.0 or greater in order to use openid wrapper'
-end
+require "openid"
 
 config.to_prepare do
   ActionController::Base.send :include, OpenidWrapper
